c503cce509d84e9e09f3ec601efc947fe9e07d19
radek <radlek19@gmail.com>
2021-10-13 10:35:27 +0200

    initial

--- src/main/java/modelo/Medicamento.java
+++ src/main/java/modelo/Medicamento.java
@@ -1,6 +1,10 @@
 package modelo;
 
-public class Medicamento {
+import java.beans.JavaBean;
+import java.io.Serializable;
+import java.text.DecimalFormat;
+
+public class Medicamento implements Serializable {
 	
 	public final static float IVA = 0.04f;
 	
@@ -12,5 +16,84 @@ public class Medicamento {
 	private int stockMinimo; // 4 bytes
 	private int codProveedor; // 4 bytes
 
+	public Medicamento(String nombre, double precio, int cod, int stock, int stockMaximo, int stockMinimo, int codProveedor) {
+		StringBuilder name=new StringBuilder(nombre);
+		name.setLength(50);
+		this.nombre = name.toString();
+		this.precio = precio;
+		this.cod = cod;
+		this.stock = stock;
+		this.stockMaximo = stockMaximo;
+		this.stockMinimo = stockMinimo;
+		this.codProveedor = codProveedor;
+	}
 
+	public Medicamento() {
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		if (nombre.length()>50){
+			throw new IllegalArgumentException(nombre+" contiene mas caracteres de los permitidos");
+		}
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = Double.parseDouble(new DecimalFormat("##.##").format(precio));
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public int getStockMaximo() {
+		return stockMaximo;
+	}
+
+	public void setStockMaximo(int stockMaximo) {
+		this.stockMaximo = stockMaximo;
+	}
+
+	public int getStockMinimo() {
+		return stockMinimo;
+	}
+
+	public void setStockMinimo(int stockMinimo) {
+		this.stockMinimo = stockMinimo;
+	}
+
+	public int getCodProveedor() {
+		return codProveedor;
+	}
+
+	public void setCodProveedor(int codProveedor) {
+		this.codProveedor = codProveedor;
+	}
+
+	@Override
+	public String toString() {
+		return "Código: "+cod+ "\nNombre: "+nombre
+				+"\nPrecio: "+ precio+"€ \nStock: "
+				+ stock+"\nProveedor: "+codProveedor ;
+	}
 }
