93e033478be2acdce0e697205d296243b926c71d
oscar <trissaikyt.g@gmail.com>
2021-10-20 17:30:02 +0200

    Ejercicioo9

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,58 +1,16 @@
 package Main;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.Random;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.DOMImplementation;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
-import org.xml.sax.SAXException;
-
-import com.thoughtworks.xstream.XStream;
-
-import dao.FarmaciaDOM;
-import dao.FarmaciaXSTREAM;
-import dao.MedicamentoAleatorio;
-import jakarta.xml.bind.JAXBContext;
-import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Marshaller;
-import jakarta.xml.bind.Unmarshaller;
-import modelo.Empleado;
-import modelo.Empresa;
-import modelo.Farmacia;
-import modelo.Medicamento;
-import modelo.Pokemon;
+import modelo.GetFacts;
 
 class Main {
-	public final static String ruta = "pokemon.json";
-
-
+	public final static String ruta = "facts.json";
 	public static void main(String[] args) {
-		Pokemon poke = new Pokemon();
-		String nombre = "ditto";
-		poke.load(nombre,ruta);
+		Random ran = new Random();
+		String num= String.valueOf(ran.nextInt(300));
+		GetFacts perro = new GetFacts();
+		perro.load(num,ruta);
 
 	}
 
--- src/main/java/modelo/GetFacts.java
+++ src/main/java/modelo/GetFacts.java
@@ -1,6 +1,7 @@
 package modelo;
 
 import com.google.common.io.Files;
+import com.google.gson.Gson;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -12,24 +13,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
 
-public class Pokemon implements Serializable {
+public class GetFacts implements Serializable {
 	private static final long serialVersionUID = 1L;
-	private static final String web = "https://pokeapi.co/api/v2/pokemon/";
+	private static final String web = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?index=";
 	private Abilitites abilities;
 	private int base_experience, height, id, weight;
 	private String name;
 	private Moves moves;
 
-	public Pokemon(Abilitites abilities, int base_experience, int height, int id, int weight, String name,
-			Moves moves) {
-		super();
-		this.abilities = abilities;
-		this.base_experience = base_experience;
-		this.height = height;
-		this.id = id;
-		this.weight = weight;
-		this.name = name;
-		this.moves = moves;
+	public GetFacts() {
 	}
 
 	public boolean load(String name, String urlArchivo) {
@@ -37,15 +29,13 @@ public class Pokemon implements Serializable {
 		Request request = new Request.Builder().url(web + name).build();
 		try (Response response = client.newCall(request).execute()) {
 
-			File file = new File(urlArchivo);
-			file.createNewFile();
 			String data = response.body().string();
-			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-			bw.write(data);
-			bw.close();
+			Gson gson = new Gson();
+			PerroFacts[] perroFact = gson.fromJson(data, PerroFacts[].class);
+			System.out.println("Dato: " + perroFact[0].getFact());
 			return true;
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.err.println("Erro al leer api");
 			return false;
 		}
 	}
