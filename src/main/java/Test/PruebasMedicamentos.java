a20136fe231bb1824a479409e3b48dbbb046b716
radek <radlek19@gmail.com>
2021-11-11 13:41:39 +0100

    arreglo junit

--- src/main/java/Test/PruebasMedicamentos.java
+++ src/main/java/Test/PruebasMedicamentos.java
@@ -1,11 +1,11 @@
 package Test;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
 
 import dao.MedicamentoAleatorio;
 import modelo.Medicamento;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
 
 public class PruebasMedicamentos {
 	MedicamentoAleatorio ma= new MedicamentoAleatorio();
@@ -13,12 +13,12 @@ public class PruebasMedicamentos {
 	@Test
 	public void testguardarMedicamento() {
 		
-		Medicamento md1=new Medicamento(0,"Paracetamol", 23, 45, 109, 1, 0);
-		Medicamento md2=new Medicamento(1,"Ibruprofeno", 16, 23, 140, 2, 1);
-		Medicamento md3=new Medicamento(2,"Acelerator", 10, 25, 137, 4, 0);
-		assertEquals(true, ma.guardar(md1));
-		assertEquals(true, ma.guardar(md2));
-		assertEquals(true, ma.guardar(md3));
+		Medicamento md1=new Medicamento("Paracetamol",0, 23, 45, 109, 1, 0);
+		Medicamento md2=new Medicamento("Ibruprofeno",1, 16, 23, 140, 2, 1);
+		Medicamento md3=new Medicamento("Acelerator",2, 10, 25, 137, 4, 0);
+		assertTrue(ma.guardar(md1));
+		assertTrue(ma.guardar(md2));
+		assertTrue(ma.guardar(md3));
 	}
 
 }
