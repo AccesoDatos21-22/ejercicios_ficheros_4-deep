273c62ecffa141fb307a67a409faecb0de72e532
radek <radlek19@gmail.com>
2021-11-11 13:47:16 +0100

    arreglo junit

--- src/main/java/Tests/MedicamentoAleatorioTest.java
+++ src/main/java/Tests/MedicamentoAleatorioTest.java
@@ -4,12 +4,6 @@ import dao.MedicamentoAleatorio;
 import modelo.Medicamento;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MedicamentoAleatorioTest {
